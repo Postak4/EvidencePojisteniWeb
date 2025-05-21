d57e29d1b0b6cb6809f747c444c8456a1c158c30
Patrik Kindl <patrik.postak@seznam.cz>
2025-05-21 16:10:08 +0200

    Odstranění referencí v Views na smazané property v PojisteniModel; Po migraci

---
+++ EvidencePojisteniWeb/Data/Migrations/20250521140726_RemovePlatnostFromPojisteniModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvidencePojisteniWeb.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePlatnostFromPojisteniModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DatumKonce",
+                table: "Pojisteni");
+
+            migrationBuilder.DropColumn(
+                name: "DatumZacatku",
+                table: "Pojisteni");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DatumKonce",
+                table: "Pojisteni",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DatumZacatku",
+                table: "Pojisteni",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
